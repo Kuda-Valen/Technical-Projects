16c941b93f192d4df19a24718aea3f958e2fe077
user <user@users-Air.lan>
2026-06-01 02:45:24 +0200

    commit 2

--- Python Projects/PyBank/main.py
+++ Python Projects/PyBank/main.py
@@ -1,1 +1,12 @@
 import json as js
+
+class User: 
+    def __init__(self, name, surname, balance, acc_type, acc_no):
+        self.name = name
+        self.surname = surname
+        self.balance = balance 
+        self.balance = acc_type
+        self.acc_no = acc_no
+
+    def add_user():
+        
